ee027fa3ea59d97dd6d4a57dcddc076e5bbb0611
Fadli Nur Zaman <fadhlynz@gmail.com>
2022-09-20 00:04:11 +0800

    fixed

--- database/seeders/DataPenyakitSeeder.php
+++ database/seeders/DataPenyakitSeeder.php
@@ -38,13 +38,13 @@ class DataPenyakitSeeder extends Seeder
       'name' => 'Penyakit Busuk Tongkol Fusarium',
       'det_penyakit' => 'Penyakit busuk tongkol Fusarium disebabkan oleh infeksi cendawan Fusarium moniliforme dan penyakit
         busuk batang jagung ini tersebar luas di Eropa, Amerika, Afrika, Australia, dan Asia.',
-      'srn_penyakit' => '<p>Pengendalian Hayati: Larutan berbasis bakteri Pseudomonas fluorescens dapat digunakan
-          sebagai perlakuan benih dan sebagai semprotan untuk mengurangi timbulnya penyakit, dan produksi racun.</p>
-        <p>Pengendalian Kimiawi: Selalu pertimbangkan pendekatan terpadu berupa tindakan pencegahan bersama dengan
+      'srn_penyakit' => 'Pengendalian Hayati: Larutan berbasis bakteri Pseudomonas fluorescens dapat digunakan
+          sebagai perlakuan benih dan sebagai semprotan untuk mengurangi timbulnya penyakit, dan produksi racun.
+        Pengendalian Kimiawi: Selalu pertimbangkan pendekatan terpadu berupa tindakan pencegahan bersama dengan
         perlakuan hayati jika tersedia. Fungisida yang diberikan awal musim dapat membatasi infeksi tongkol. Karena
         kerusakan terjadi di tongkol, fungisida bukan cara paling efektif untuk melawan penyakit ini. Pertimbangkan
         untuk mengendalikan hama serangga yang melukai tongkol dan mendukung pertumbuhan jamur. Produk yang mengandung
-        propikonazol 1 ml / I dapat digunakan pada tahap pengerasan biji untuk mengendalikan jamur.</p>',
+        propikonazol 1 ml / I dapat digunakan pada tahap pengerasan biji untuk mengendalikan jamur.',
       'images' => '1662961206.png',
     ]);
     Penyakit::create([
@@ -52,14 +52,14 @@ class DataPenyakitSeeder extends Seeder
       'code' => 'P4',
       'name' => 'Penyakit Karat Daun',
       'det_penyakit' => 'Penyakit karat adalah segolongan penyakit tumbuhan yang disebabkan oleh golongan cendawan (fungi) yang termasuk dalam bangsa (ordo) Pucciniales. Penyakit ini paling jelas gejalanya terlihat pada daun, sehingga disebut karat daun.',
-      'srn_penyakit' => '<p>Pengendalian Kimiawi: Selalu pertimbangkan pendekatan terpadu dengan tindakan pencegahan
+      'srn_penyakit' => 'Pengendalian Kimiawi: Selalu pertimbangkan pendekatan terpadu dengan tindakan pencegahan
             bersama dengan perlakuan hayati jika tersedia. Penyemprotan fungisida dapat bermanfaat jika digunakan pada
             varietas yang rentan. Berikan fungisida daun pada awal musim jika karat menyebar dengan cepat karena kondisi
             cuaca. Banyak jenis fungisida yang tersedia untuk pengendalian karat. Produk-produk yang mengandung
             mankozeb, piraklostrobin, piraklostrobin + metkonazol, piraklostrobin fluksapiroksad, azoksistrobin +
             propikonazol, trifloksistrobin + protiokonazol dapat digunakan untuk mengendalikan penyakit ini. Contoh
             perlakuannya dapat berupa penyemprotan mankozeb @ 2,5 g/l segera setelah bintil-bintil muncul dan ulangi
-            dalam selang 10 hari hingga tahap berbunga.</p>',
+            dalam selang 10 hari hingga tahap berbunga.',
       'images' => '1662961220.png',
     ]);
     Penyakit::create([
@@ -67,11 +67,7 @@ class DataPenyakitSeeder extends Seeder
       'code' => 'P5',
       'name' => 'Penyakit Virus Mosaik Kerdil Jagung',
       'det_penyakit' => 'Penyakit ini merupakan penyakit virus jagung yang pertama dilaporkan di Indonesia. Penyebarannya sangat luas, meliputi hampir di semua negara penghasil jagung di dunia.',
-      'srn_penyakit' => '<p>1. Mencabut tanaman yang terinfeksi seawal mungkin agar tidak menjadi sumber infeksi bagi
-          tanaman sekitarnya ataupun pertanaman yang akan datang.</p><p>2. Mengadakan pergiliran tanaman, tidak menanam
-      jagung terus menerus di lahan yang sama.</p><p>3. Penggunaan peptisida apabila di lapangan populasi vektor cukup
-      tinggi.</p>
-      <p>4. Tidak penggunakan benih yang berasal dari tanaman yang terinfeksi.</p>',
+      'srn_penyakit' => '1) Mencabut tanaman yang terinfeksi seawal mungkin agar tidak menjadi sumber infeksi bagi tanaman sekitarnya ataupun pertanaman yang akan datang. 2) Mengadakan pergiliran tanaman, tidak menanam jagung terus menerus di lahan yang sama. 3) Penggunaan peptisida apabila di lapangan populasi vektor cukup tinggi. 4) Tidak penggunakan benih yang berasal dari tanaman yang terinfeksi.',
       'images' => '1662961233.png',
     ]);
   }
